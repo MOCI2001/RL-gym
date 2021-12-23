22db9396366b31d5da56bc53fe29588cec6593fc
Richard Kuo <rkuo2000@gmail.com>
2021-12-23 15:47:14 +0800

    Add files via upload

---
+++ SB3/cartpole_ppo.py
@@ -0,0 +1,8 @@
+from stable_baselines3 import PPO
+from stable_baselines3.common.env_util import make_vec_env
+
+env = make_vec_env("CartPole-v1", n_envs=4) # training in parallel environments
+
+model = PPO("MlpPolicy", env, verbose=1)
+model.learn(total_timesteps=25000)
+model.save("ppo_cartpole")
