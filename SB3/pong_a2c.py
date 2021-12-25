6ea581fa80c44920793484f1724e9efe6a1be491
Richard Kuo <rkuo2000@gmail.com>
2021-12-26 03:07:54 +0800

    Add files via upload

---
+++ SB3/pong_a2c.py
@@ -0,0 +1,28 @@
+from stable_baselines3 import A2C
+from stable_baselines3.common.env_util import make_atari_env
+
+env = make_atari_env('PongNoFrameskip-v4', n_envs=4)
+
+model = A2C('MlpPolicy', env, verbose=1)
+model.learn(total_timesteps=10000)
+model.save("a2c_pong")
+
+## load model and continue the training
+model = A2C.load("a2c_pong", verbose=1)
+env = make_atari_env('PongNoFrameskip-v4', n_envs=4)
+model.set_env(env)
+model.learn(int(0.5e6))
+model.save("a2c_pong")
+
+del model # remove to demonstrate saving and loading
+
+## replay
+#env = model.get_env()
+env = make_atari_env('PongNoFrameskip-v4', n_envs=1)
+model = A2C.load("a2c_pong")
+
+obs = env.reset()
+while True:
+    action, _states = model.predict(obs)
+    obs, rewards, dones, info = env.step(action)
+    env.render()
