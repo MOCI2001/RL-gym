352d5f2966f4abd39880820b553b4a66e0027215
Richard Kuo <rkuo2000@gmail.com>
2021-12-26 04:27:56 +0800

    Update pong_a2c.py

--- SB3/pong_a2c.py
+++ SB3/pong_a2c.py
@@ -3,7 +3,7 @@ from stable_baselines3.common.env_util import make_atari_env
 
 env = make_atari_env('PongNoFrameskip-v4', n_envs=4)
 
-model = A2C('MlpPolicy', env, verbose=1)
+model = A2C('CnnPolicy', env, verbose=1)
 model.learn(total_timesteps=10000)
 model.save("a2c_pong")
 
