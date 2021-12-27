49dbbf5410d0aaa6878a37419fb60667828c4474
Richard Kuo <rkuo2000@gmail.com>
2021-12-28 02:41:54 +0800

    Delete SB3 directory

--- SB3/breakout_ppo.py
+++
@@ -1,9 +0,0 @@
-from stable_baselines3 import PPO
-from stable_baselines3.common.env_util import make_atari_env
-
-env = make_atari_env('Breakout-v4', n_envs=4)
-
-## Create & Train Agent
-model = PPO('MlpPolicy', env, verbose=1)
-model.learn(total_timesteps=int(1e6))
-model.save("ppo_breakout")
--- SB3/cartpole_dqn.py
+++
@@ -1,8 +0,0 @@
-from stable_baselines3 import DQN
-from stable_baselines3.common.env_util import make_vec_env
-
-env = make_vec_env("CartPole-v1", n_envs=1) # DQN only for single environment
-
-model = DQN("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=25000, log_interval=4)
-model.save("dqn_cartpole")
